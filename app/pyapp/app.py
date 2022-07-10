d60099a05d0f63b55e33f3212f7b6e7cdd5e2813
shoma564 <shomaigu@gmail.com>
2022-07-10 03:25:53 +0000

    socket

--- app/pyapp/app.py
+++ app/pyapp/app.py
@@ -1,14 +1,55 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 import PIL.Image, PIL.ImageDraw, PIL.ImageFont
 
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+# ユーザー数
+user_count = 0
+# 現在のテキスト
+text = ""
+
+# ユーザーが新しく接続すると実行
+@socketio.on('connect')
+def connect(auth):
+    global user_count, text
+    user_count += 1
+    # 接続者数の更新（全員向け）
+    emit('count_update', {'user_count': user_count}, broadcast=True)
+    # テキストエリアの更新
+    emit('text_update', {'text': text})
+
+
+# ユーザーの接続が切断すると実行
+@socketio.on('disconnect')
+def disconnect():
+    global user_count
+    user_count -= 1
+    # 接続者数の更新（全員向け）
+    emit('count_update', {'user_count': user_count}, broadcast=True)
+
+
+# テキストエリアが変更されたときに実行
+@socketio.on('text_update_request')
+def text_update_request(json):
+    global text
+    text = json["text"]
+    # 変更をリクエストした人以外に向けて送信する
+    # 全員向けに送信すると入力の途中でテキストエリアが変更されて日本語入力がうまくできない
+    emit('text_update', {'text': text}, broadcast=True, include_self=False)
+
+
+
+
 
 @app.route('/')
 def index():
     global name
-    return render_template("index.html", name = name)
+    return render_template("index.html",name = name)
 
 
 
@@ -30,6 +71,12 @@ def room():
 def show():
     message = "hello world"
     return render_template("form.html", message = message)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- db/data/sys/session.frm
+++ db/data/sys/session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2022-07-09 16:03:19
+timestamp=2022-07-10 01:04:34
 create-version=1
 source=SELECT * FROM sys.processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
